c279b9e38a5c11715081947795af374c69e74f92
Flutter-Code-Craft <muneefullah24mdswe363@gmail.com>
2026-08-25 03:15:20 -0700

    add all files

---
+++ lib/Splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SPLASHSCREEN extends StatefulWidget {
+  const SPLASHSCREEN({super.key});
+
+  @override
+  State<SPLASHSCREEN> createState() => _SPLASHSCREENState();
+}
+
+class _SPLASHSCREENState extends State<SPLASHSCREEN> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
+
